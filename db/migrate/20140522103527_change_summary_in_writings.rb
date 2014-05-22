c25357a086f64472227cfddeea8922c508d40d85
madebyeggs <madebyeggs@gmail.com>
2014-05-22 15:26:08 +0100

    blog summary length change

--- db/migrate/20140522103527_change_summary_in_writings.rb
+++ db/migrate/20140522103527_change_summary_in_writings.rb
@@ -1,7 +1,10 @@
 class ChangeSummaryInWritings < ActiveRecord::Migration
   def up
+      change_column :writings, :summary, :text
   end
-
   def down
+      # This might cause trouble if you have strings longer
+      # than 255 characters.
+      change_column :writings, :summary, :string
   end
 end
