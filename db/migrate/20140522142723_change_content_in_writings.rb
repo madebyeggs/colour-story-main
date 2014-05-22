7556ae74e1e231b2c852a3c98d3b8aeb37c64ea9
madebyeggs <madebyeggs@gmail.com>
2014-05-22 15:27:52 +0100

    blog content length change

---
+++ db/migrate/20140522142723_change_content_in_writings.rb
@@ -0,0 +1,10 @@
+class ChangeContentInWritings < ActiveRecord::Migration
+  def up
+      change_column :writings, :content, :text
+  end
+  def down
+      # This might cause trouble if you have strings longer
+      # than 255 characters.
+      change_column :writings, :content, :string
+  end
+end
