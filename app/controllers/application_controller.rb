a972af86048b59110075e6647a240b485b315d63
madebyeggs <madebyeggs@gmail.com>
2014-07-03 13:12:02 +0100

    blog feed fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def bring_in_models
-    @feeds = Writing.find(:all, :order => "id ASC", :limit => 10)
+    @feeds = Writing.find(:all, :order => "id DESC", :limit => 10)
   end
   
   def bring_in_tweets
